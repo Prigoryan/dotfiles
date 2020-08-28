0c204e25f792c54acce3679b49921ad87d2fe506
Prigoryan <itsforspam99@gmail.com>
2020-08-28 17:00:01 +0600

    Update via cron: 2020-08-28 17:00:01

--- bin/wait-append-ws.sh
+++ bin/wait-append-ws.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sleep 1
 ws1=$1
-i3-msg "workspace $ws1; append_layout ~/.i3/workspace-1-2.json; exec konsole -e zsh -is eval htop; exec notepadqq"
+i3-msg "workspace $ws1; append_layout ~/.i3/workspace-1-2.json; exec konsole -e zsh -is eval htop; exec subl3"
