60ddd47b3b10f47d635713402b8c87bdb24f8c96
Prigoryan <itsforspam99@gmail.com>
2020-10-03 23:00:01 +0600

    Update via cron: 2020-10-03 23:00:01

--- bin/wait-append-ws.sh
+++ bin/wait-append-ws.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sleep 1
 ws1=$1
-i3-msg "workspace $ws1; append_layout ~/.i3/workspace-1-2.json; exec konsole -e zsh -is eval 'sleep 1 && htop'; exec subl3"
+i3-msg "workspace $ws1; append_layout ~/.i3/workspace-1-2.json; exec konsole -e zsh -is eval htop; exec subl3"
