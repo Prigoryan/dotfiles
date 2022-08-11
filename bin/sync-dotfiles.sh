d8928472fca37064f0c3fc0725d38ef5d010b953
Prigoryan <itsforspam99@gmail.com>
2022-08-11 23:03:15 +0600

    Update via cron: 2022-08-11 23:03:15

--- bin/sync-dotfiles.sh
+++ bin/sync-dotfiles.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd ~/dotfiles
-eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && git add . && git commit -m "Update via cron: `date +\"%Y-%m-%d %H:%M:%S\"`" && git push
+git add . && git commit -m "Update via cron: `date +\"%Y-%m-%d %H:%M:%S\"`" && git push
