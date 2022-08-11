f458f0eeafa3c04984db49e9b5a9fd1039d4b257
Prigoryan <itsforspam99@gmail.com>
2022-08-11 23:05:38 +0600

    Update via cron: 2022-08-11 23:05:38

--- bin/sync-dotfiles.sh
+++ bin/sync-dotfiles.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd ~/dotfiles
-git add . && git commit -m "Update via cron: `date +\"%Y-%m-%d %H:%M:%S\"`" && git push
+git add . && git commit -m "Update via cron: `date +\"%Y-%m-%d %H:%M:%S\"`" && git push || git push
