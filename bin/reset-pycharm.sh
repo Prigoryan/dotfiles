a97de49212eb8f3a64b123a281bf2624c0787241
Prigoryan <itsforspam99@gmail.com>
2025-12-08 16:00:00 +0600

    Update via cron: 2025-12-08 16:00:00

--- bin/reset-pycharm.sh
+++ bin/reset-pycharm.sh
@@ -4,19 +4,19 @@ for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand Ruby
     echo "[+] Resetting trial period for $product"
 
     echo "[+] Removing Evaluation Key..."
-    rm -rf ~/.config/$product*/eval
+    rm -rf ~/.config/$product*/eval 2> /dev/null
 
     # Above path not working on latest version, Fixed below
-    rm -rf ~/.config/JetBrains/$product*/eval
+    rm -rf ~/.config/JetBrains/$product*/eval 2> /dev/null
 
     echo "[+] Removing all evlsprt properties in options.xml..."
-    sed -i 's/evlsprt//' ~/.config/$product*/options/other.xml
+    sed -i 's/evlsprt//' ~/.config/$product*/options/other.xml 2> /dev/null
 
     # Above path not working on latest version, Fixed below
-    sed -i 's/evlsprt//' ~/.config/JetBrains/$product*/options/other.xml
+    sed -i 's/evlsprt//' ~/.config/JetBrains/$product*/options/other.xml 2> /dev/null
 
     echo
 done
 
 echo "Removing userPrefs files..."
-rm -rf ~/.java/.userPrefs
+rm -rf ~/.java/.userPrefs 2> /dev/null
