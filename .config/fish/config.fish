48bd242273d49a81ce2a9080193b00c85c40eb89
Prigoryan <itsforspam99@gmail.com>
2026-05-24 03:20:00 +0600

    Update via cron: 2026-05-24 03:20:00

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    
+
     # fzf --fish | source
+    source ~/.config/fish/fzf.sh
 
     # fzf_key_bindings
 
-    
-
     set -gx fifc_editor micro
     set -gx fifc_fd_opts --hidden
-    set -gx _fifc_custom_fzf_opts '--bind=up:up,down:down'
     
     set -gx fish_greeting
     
