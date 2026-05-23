87bb05c41302b7a37d5bda63db266433ab8c9517
Prigoryan <itsforspam99@gmail.com>
2026-05-24 02:20:00 +0600

    Update via cron: 2026-05-24 02:20:00

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fzf --fish | source
+    
+    # fzf --fish | source
 
+    # fzf_key_bindings
+
+    
+
+    set -gx fifc_editor micro
+    set -gx fifc_fd_opts --hidden
+    set -gx _fifc_custom_fzf_opts '--bind=up:up,down:down'
+    
+    set -gx fish_greeting
+    
     bind ctrl-h backward-kill-path-component
 end
